1efd56541506faffdc2ccab91f5654f09b696a61
linqiao <linqiao@DEXIAO-CN.oradev.oraclecorp.com>
2016-03-17 18:19:38 +0800

    change file name

--- connectorserviceTest/src/main/java/com/oracle/bdd/connectorserviceTest/ConnectsTest.java
+++ connectorserviceTest/src/main/java/com/oracle/bdd/connectorserviceTest/ConnectsTest.java
@@ -9,7 +9,7 @@ import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
 
-public class ConnectsAPITest {
+public class ConnectsTest {
 
 	@Test
 	public void test_firstC() {
