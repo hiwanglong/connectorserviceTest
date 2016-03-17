a7bd9ea9192b646013964880cfff61f2bebf7d2c
stellaw <stellaw@abc.com>
2016-03-17 14:25:47 +0800

    first commit

---
+++ connectorserviceTest/src/main/java/com/oracle/bdd/connectorserviceTest/App.java
@@ -0,0 +1,13 @@
+package com.oracle.bdd.connectorserviceTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
