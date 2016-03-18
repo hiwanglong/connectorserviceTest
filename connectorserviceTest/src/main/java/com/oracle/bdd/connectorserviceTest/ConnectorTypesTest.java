90c01dba71cba2f4073a4a203cea89facb3f2dd9
stellaw <stellaw@abc.com>
2016-03-18 10:52:15 +0800

    create connectortypestest.xml

--- connectorserviceTest/src/main/java/com/oracle/bdd/connectorserviceTest/ConnectorTypesTest.java
+++ connectorserviceTest/src/main/java/com/oracle/bdd/connectorserviceTest/ConnectorTypesTest.java
@@ -2,6 +2,9 @@ package com.oracle.bdd.connectorserviceTest;
 
 import static org.junit.Assert.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -10,6 +13,7 @@ import org.junit.Test;
 
 import com.oracle.bdd.util.Constants;
 import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
 
 public class ConnectorTypesTest {
@@ -42,8 +46,38 @@ public class ConnectorTypesTest {
 		String conn=Constants.connectorUrl+"connectorTypes";
 		System.out.println(conn);
 		
+		
+		
+				
+
+		
+		
+		
         Client client = Client.create();
+                
         WebResource webRes = client.resource(conn);
+        
+       /* ClientResponse response = webRes.accept("application/json").get(ClientResponse.class);
+        
+        if (response.getStatus() != 200) {
+ 		   throw new RuntimeException("Failed : HTTP error code : "
+ 			+ response.getStatus());
+ 		}
+
+ 		String output = response.getEntity(String.class);
+ 		
+
+ 		System.out.println("Output from Server .... \n");
+ 		System.out.println(output);
+        */
+        
+        
+        
+        System.out.println( webRes.getProperties());
+        System.out.println(webRes.queryParam("name", "CloudStorage"));
+        
+        
+        System.out.println(webRes.entity(String.class));
         
         // check status 200
         int status=webRes.head().getStatus();
@@ -51,6 +85,7 @@ public class ConnectorTypesTest {
         assertEquals(200, status);
         
         // check response "cloud" "HDFS"
+        
         String res = webRes.acceptLanguage("en-US").get(String.class);
         System.out.println(res);
         //assert...?
