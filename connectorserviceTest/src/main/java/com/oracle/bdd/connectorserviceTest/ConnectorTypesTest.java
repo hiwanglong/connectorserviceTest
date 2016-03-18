2218c2fbe272b8a32eb96fb417f499c6ef98e959
stellaw <stellaw@abc.com>
2016-03-18 13:09:49 +0800

    update connectorType test case

--- connectorserviceTest/src/main/java/com/oracle/bdd/connectorserviceTest/ConnectorTypesTest.java
+++ connectorserviceTest/src/main/java/com/oracle/bdd/connectorserviceTest/ConnectorTypesTest.java
@@ -2,7 +2,6 @@ package com.oracle.bdd.connectorserviceTest;
 
 import static org.junit.Assert.*;
 
-import java.util.HashMap;
 import java.util.Map;
 
 import org.junit.After;
@@ -12,14 +11,23 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.oracle.bdd.util.Constants;
+import com.oracle.bdd.util.GetResourceXML;
 import com.sun.jersey.api.client.Client;
-import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
 
 public class ConnectorTypesTest {
+	
+	static WebResource webRes;
+	static String res;
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
+		String conn=Constants.connectorUrl+"connectorTypes";
+		System.out.println(conn);	
+        Client client = Client.create();
+        webRes = client.resource(conn);
+        res = webRes.accept("application/json").get(String.class);
+        System.out.println(res);
 	}
 
 	@AfterClass
@@ -28,69 +36,26 @@ public class ConnectorTypesTest {
 
 	@Before
 	public void setUp() throws Exception {
+
 	}
 
 	@After
 	public void tearDown() throws Exception {
 	}
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
 
 	@Test
-	public void testConnectorTypes() {
-
-		//String conn=Contants.getConnectorserviceUrl()+"connectorTypes";
-		String conn=Constants.connectorUrl+"connectorTypes";
-		System.out.println(conn);
-		
-		
-		
-				
-
-		
-		
-		
-        Client client = Client.create();
-                
-        WebResource webRes = client.resource(conn);
-        
-       /* ClientResponse response = webRes.accept("application/json").get(ClientResponse.class);
-        
-        if (response.getStatus() != 200) {
- 		   throw new RuntimeException("Failed : HTTP error code : "
- 			+ response.getStatus());
- 		}
-
- 		String output = response.getEntity(String.class);
- 		
-
- 		System.out.println("Output from Server .... \n");
- 		System.out.println(output);
-        */
-        
-        
-        
-        System.out.println( webRes.getProperties());
-        System.out.println(webRes.queryParam("name", "CloudStorage"));
-        
-        
-        System.out.println(webRes.entity(String.class));
-        
-        // check status 200
+	public void testGetConnectorTypes() {              
         int status=webRes.head().getStatus();
+        if(status!=200){ 
+        	fail("Fail since status is "+status);
+        }
         System.out.println(status);
-        assertEquals(200, status);
-        
-        // check response "cloud" "HDFS"
-        
-        String res = webRes.acceptLanguage("en-US").get(String.class);
-        System.out.println(res);
-        //assert...?
-
-
-		//fail("Not yet implemented");
+        Map<String, String> res_map=GetResourceXML.parseXml("ConnectorTypesTest.xml", "testGetConnectorTypes");
+        String res_expected=res_map.get("RESPONSEJSON").trim();
+      	assertTrue("ConnectorTypes response is NOT as expected",res.equals(res_expected));
+            
 	}
+	
+	
 }
