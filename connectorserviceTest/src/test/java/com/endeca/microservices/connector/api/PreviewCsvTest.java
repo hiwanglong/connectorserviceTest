4127c6a1472b560196c9f91fce155caed593b502
linqiao <linqiao@DEXIAO-CN.oradev.oraclecorp.com>
2016-03-30 15:41:19 +0800

    Signed-off-by: linqiao <linqiao@DEXIAO-CN.oradev.oraclecorp.com>

--- connectorserviceTest/src/test/java/com/endeca/microservices/connector/api/PreviewCsvTest.java
+++ connectorserviceTest/src/test/java/com/endeca/microservices/connector/api/PreviewCsvTest.java
@@ -12,50 +12,49 @@ import com.sun.jersey.api.client.Client;
 
 public class PreviewCsvTest {
 	private static Client client = Client.create();;
-	private String reqUrl = Constants.connectors;
-	private String testName;
-	private Map<String, String> responseMap ;
-	private String connectorId;
+	private static String reqUrl = Constants.connectors;
+	private static String testName;
+	private static Map<String, String> responseMap ;
+	private static String connectorId;
+	private static String token;
+	private static String xmlName = "PreviewCsvTest.xml";
 	private String containerId;
-	private String token;
-	private String xmlName = "PreviewCsvTest.xml";
 	
 	
-	CommonUtil comUtil = new CommonUtil(client, xmlName, BrowseContainerByIdTest.class);
+	private static CommonUtil comUtil = new CommonUtil(client, xmlName, PreviewCsvTest.class);
 	
 	@BeforeClass(alwaysRun = true)
 	public static void setUpBeforeClass() throws Exception {
-		
-		
-	}
-	
-	@AfterMethod(alwaysRun = true)
-	public void tearDown() throws Exception {
-		//delete connector by id after each test
-		comUtil.executeDelete(Constants.connectors+"/"+connectorId);	
-	}
-	
-	/**
-	 * preview csv file with containerId
-	 */
-	@Test(groups = {"Functional"})
-	public void testProviewCsvByContainerId1() {	
 		//post connector		
 		testName = "previewCsv-connector";	
 		responseMap = comUtil.executePost(reqUrl,testName);	//create connector and get response					
 		connectorId = comUtil.getConnectorId(responseMap.get("jsonRes")).get(0);	//get connector id
 		
 		//post auth	
-		testName = "proviewCsvByContainerId1-auth";		
+		testName = "previewCsv-auth";		
 		reqUrl = Constants.connectorAuth.replace("{connectorId}",connectorId);	//replace connectorId to get url
 		responseMap = comUtil.executePost(reqUrl,testName);				//post auth
 		token = comUtil.getToken(responseMap.get("jsonRes"));			//get token
 		
+		System.out.println(token);
+	}
+	
+	@AfterMethod(alwaysRun = true)
+	public void tearDown() throws Exception {
+		//delete connector by id after each test
+		//comUtil.executeDelete(Constants.connectors+"/"+connectorId);	
+	}
+	
+	/**
+	 * preview csv file with containerId
+	 */
+	@Test(groups = {"Functional"})
+	public void testProviewCsvByContainerId1() {		
 		//proview
-		testName = "proviewCsvByContainerId1-proview";
-		containerId = comUtil.getXmlNode(testName, "CONTAINERID");		//get containerId
-		reqUrl = Constants.browseByContainerId.replace("{connectorId}",connectorId).replace("{containerId}",containerId);	//replace connectorId & containerId to get url
-		responseMap = comUtil.executeGet(reqUrl, token);				//execute browse
+		testName = "previewCsvByContainerId1";
+		reqUrl = Constants.previewData.replace("{connectorId}",connectorId);	//replace connectorId to get url
+		responseMap = comUtil.executePost(reqUrl, testName, token);				//execute preview
+		
 		
 		comUtil.checkStatus(responseMap, testName);
 		comUtil.checkResponseRegex(responseMap, "browseContainerById2-browse");			//check response with REGEX
